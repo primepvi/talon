31ecd422a1bc47eb56e1fe0498fc740c1c489e62
primepvi <pedrobernardesv@gmail.com>
2026-02-28 19:28:09 -0300

    chore: creating node_id environment variable.

--- config/config.exs
+++ config/config.exs
@@ -4,4 +4,5 @@ config :talon,
   docker_host: System.get_env("DOCKER_HOST", "http://localhost:2375/"),
   repo_directory: System.get_env("REPO_DIRECTORY", "./talon/"),
   panel_url: System.get_env("PANEL_URL", "ws://localhost:9000/"),
-  panel_token: System.get_env("PANEL_TOKEN", "banana")
+  panel_token: System.get_env("PANEL_TOKEN", "banana"),
+  node_id: System.get_env("NODE_ID", "banana")
