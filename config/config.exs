3d7e812d289f780a8656d24fd70179b835a50b28
primepvi <pedrobernardesv@gmail.com>
2026-03-05 14:58:46 -0300

    chore: modifying config for some panel tests.

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,6 @@ import Config
 config :talon,
   docker_host: System.get_env("DOCKER_HOST", "http://localhost:2375/"),
   repo_directory: System.get_env("REPO_DIRECTORY", "./talon/"),
-  panel_url: System.get_env("PANEL_URL", "ws://localhost:9000/"),
+  panel_url: System.get_env("PANEL_URL", "http://localhost:3030/panel/ws"),
   panel_token: System.get_env("PANEL_TOKEN", "banana"),
   node_id: System.get_env("NODE_ID", "banana")
